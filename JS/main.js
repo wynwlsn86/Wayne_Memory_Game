e588fdc172ae129761df8616068504160e45b214
Wayne Wilson <wynwlsn86@gmail.com>
2019-03-02 21:54:00 -0500

    created logic game

--- JS/main.js
+++ JS/main.js
@@ -3,18 +3,24 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 let cardOne;
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-let cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
-  if(cardsInPlay[2] === cardsInPlay[3]){
-    window.alert('You found a match!');
-  }
-  else{
-    window.alert('Try again!');
+function checkForMatch () {
+  if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+      window.alert("You found a match!");
+    }
+    else{
+      window.alert("Try again!");
+    }
   }
 }
-console.log("User flipped " + cardsInPlay[1])
+
+function flipCard (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+
+
+flipCard(0);
+flipCard(2);
