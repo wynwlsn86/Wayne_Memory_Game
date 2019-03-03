c1f7f14d8e3ac6a8505f4e2cd3f079ef6b6c82c0
Wayne Wilson <wynwlsn86@gmail.com>
2019-03-02 21:20:25 -0500

    created logic game

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,20 @@
-console.log("Up and Running!");
 
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
 
-console.log("User flipped " + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[2] === cardsInPlay[3]){
+    window.alert('You found a match!');
+  }
+  else{
+    window.alert('Try again!');
+  }
+}
+console.log("User flipped " + cardsInPlay[1])
